815f08e196fc5b84a075b6ce1d3752ba253f34db
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-03-14 21:59:31 -0300

    Criação dos models e da data das screens

---
+++ lib/data/ScreensData.dart
@@ -0,0 +1,12 @@
+import '../models/screenInformationsClass.dart';
+
+const ScreenData = const [
+  ScreenInformation(
+    id: 1,
+    title: 'ControleDespesas',
+  ),
+  ScreenInformation(
+    id: 2,
+    title: 'Planner',
+  )
+];
